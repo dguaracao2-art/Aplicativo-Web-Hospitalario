cd5085e1bdd5c65290fd02ae7b450678478d4d15
Danny Guaraca <dguaracao2@unemi.edu.ec>
2026-07-09 15:34:18 -0500

    Reorganiza la estructura del proyecto

---
+++ backend/config/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import include, path
+from security.views import InicioTemplate
+from core.views import DashboardView
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", DashboardView.as_view(), name="home"),
+    path("security/", include("security.urls")),
+    path("catalog/", include("catalog.urls")),
+    path("customers/", include("customers.urls")),
+    path("invoicing/", include("invoicing.urls")),
+]
